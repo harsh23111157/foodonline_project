6184483dff75f52166dfe132d2af157959605ecf
harshal patil <harshalpatil6860@gmail.com>
2026-08-01 14:24:32 +0530

    vendor and customer dashboard architecture edits

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,22 +1,25 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 
 urlpatterns=[
-  path('registerUser',views.registerUser,name='registerUser'),
-  path('registerVendor',views.registerVendor,name='registerVendor'),
+  path('',views.myAccount),
+  path('registerUser/',views.registerUser,name='registerUser'),
+  path('registerVendor/',views.registerVendor,name='registerVendor'),
 
-  path('login', views.login, name='login'), 
-  path('logout',views.logout,name='logout'),
-  path('myAccount',views.myAccount,name='myAccount'),
-  path('custDashboard',views.custDashboard,name='custDashboard'),
-  path('vendorDashboard',views.vendorDashboard,name='vendorDashboard'),
+  path('login/', views.login, name='login'), 
+  path('logout/',views.logout,name='logout'),
+  path('myAccount/',views.myAccount,name='myAccount'),
+  path('custDashboard/',views.custDashboard,name='custDashboard'),
+  path('vendorDashboard/',views.vendorDashboard,name='vendorDashboard'),
 
   path('activate/<str:uid64>/<str:token>/', views.activate, name='activate'),
 
   path('forgot_password/', views.forgot_password, name='forgot_password'),
   path('reset_password_validate/<str:uid64>/<str:token>/', views.reset_password_validate, name='reset_password_validate'),
   path('reset_password/', views.reset_password, name='reset_password'),
+
+  path('vendor/',include('vendor.urls')),
 
   
 
--- vendor/views.py
+++ vendor/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . import views
 
 # Create your views here.
+
+def vprofile(request):
+    return render(request,'vendor/vprofile.html')
